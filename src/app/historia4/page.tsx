31ad11d099955e8e410b0b4b9cc69096a69516b0
Hectorhr12 <202206428@est.umss.edu>
2025-04-24 15:25:26 -0400

    Update tabla historial page.tsx

--- src/app/historia4/page.tsx
+++ src/app/historia4/page.tsx
@@ -3,10 +3,9 @@
 import { ProductosTable } from "@/components/ui/historia4/ProductosTable";
 import Header from "@/components/ui/Header";
 import { useEffect, useState } from "react";
-
+import Link from "next/link";
 export default function Historia4Page() {
   const [hostId, setHostId] = useState<number | null>(null);
-
   useEffect(() => {
     const id_usuarioHost=4
   setHostId(id_usuarioHost); 
@@ -15,9 +14,18 @@ export default function Historia4Page() {
     <div>
       <Header />
       <div className="p-8">
-        <h1 className="text-3xl font-bold mb-6">Actividad de mis vehiculos</h1>
+        <h1 className="text-3xl font-bold mb-6">Historial de Reservaciones</h1>
         {hostId ? (
-          <ProductosTable hostId={hostId} />
+          <>
+        <ProductosTable hostId={hostId} />
+          <div className="flex justify-end mt-4">
+         <Link href="/">
+          <button className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700">
+            Atrás
+          </button>
+          </Link>
+        </div>
+          </>
         ) : (
           <div className="text-center py-8">Cargando información del host...</div>
         )}
